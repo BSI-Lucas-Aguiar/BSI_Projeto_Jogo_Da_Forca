49784608676a4ebd3501d5daec40106756f5051b
Lucas de Figueiredo Aguiar <37455976+sendrak@users.noreply.github.com>
2022-06-05 19:34:06 -0300

    Atualização no Boneco

--- Projeto_Forca/src/br/edu/iff/jogoforca/dominio/boneco/texto/BonecoTexto.java
+++ Projeto_Forca/src/br/edu/iff/jogoforca/dominio/boneco/texto/BonecoTexto.java
@@ -7,138 +7,107 @@ public class BonecoTexto implements Boneco {
 	private static BonecoTexto  soleInstance = null;
 
 	public static BonecoTexto getSoleInstance() {
-        if (soleInstance == null) {
-            soleInstance = new BonecoTexto();
-        }
+		if (soleInstance == null) {
+			soleInstance = new BonecoTexto();
+		}
 
-        return soleInstance;
-    }
-	
-	
-	private BonecoTexto() {
-		
+		return soleInstance;
 	}
-	
+
+
+	private BonecoTexto() {
+
+	}
+
 	@Override
 	public void exibir(Object object, int partes) {
 		if (partes == 1) {
-			System.out.println("------------------------");
 			System.out.println("|----------------------|");
 			System.out.println("|----------------------|");
-			System.out.println("|                      cabeca");
-			
+			System.out.println("|                   (     )");
+
 		}
-		
+
 		if (partes == 2) {
-			
-			System.out.println("------------------------");
 			System.out.println("|----------------------|");
 			System.out.println("|----------------------|");
-			System.out.println("|                      cabeca");
-			System.out.println("|                olho direito");
-					
+			System.out.println("|                   (*    )");
+
 		}
-		
+
 		if (partes == 3) {
-			System.out.println("------------------------");
 			System.out.println("|----------------------|");
 			System.out.println("|----------------------|");
-			System.out.println("|                      cabeca");
-			System.out.println("|                olho direito");
-			System.out.println("|               olho esquerdo");
+			System.out.println("|                   (*   *)");
 		}
-		
+
 		if (partes == 4) {
-			System.out.println("------------------------");
 			System.out.println("|----------------------|");
 			System.out.println("|----------------------|");
-			System.out.println("|                      cabeca");
-			System.out.println("|                olho direito");
-			System.out.println("|               olho esquerdo");
-			System.out.println("|                       nariz");
+			System.out.println("|                   (* - *)");
 		}
-		
-		
+
+
 		if (partes == 5) {
-			System.out.println("------------------------");
 			System.out.println("|----------------------|");
 			System.out.println("|----------------------|");
-			System.out.println("|                      cabeca");
-			System.out.println("|                olho direito");
-			System.out.println("|               olho esquerdo");
-			System.out.println("|                       nariz");
-			System.out.println("|                        boca");
+			System.out.println("|                   (* - *)");
+			System.out.println("|                      o ");
 		}
-		
+
 		if (partes == 6) {
-			System.out.println("------------------------");
 			System.out.println("|----------------------|");
 			System.out.println("|----------------------|");
-			System.out.println("|                      cabeca");
-			System.out.println("|                olho direito");
-			System.out.println("|               olho esquerdo");
-			System.out.println("|                       nariz");
-			System.out.println("|                        boca");
-			System.out.println("|                      tronco");
+			System.out.println("|                   (* - *)");
+			System.out.println("|                      o ");
+			System.out.println("|                      | ");
+			System.out.println("|                      |  ");
+			System.out.println("|                      -  ");
 		}
-		
+
 		if (partes == 7) {
-			System.out.println("------------------------");
 			System.out.println("|----------------------|");
 			System.out.println("|----------------------|");
-			System.out.println("|                      cabeca");
-			System.out.println("|                olho direito");
-			System.out.println("|               olho esquerdo");
-			System.out.println("|                       nariz");
-			System.out.println("|                        boca");
-			System.out.println("|                      tronco");
-			System.out.println("|               braco direito");
-					
+			System.out.println("|                   (* - *)");
+			System.out.println("|                      o ");
+			System.out.println("|                  ___ | ");
+			System.out.println("|                      |  ");
+			System.out.println("|                      -  ");
+
 		}
-		
+
 		if (partes == 8) {
-			System.out.println("------------------------");
 			System.out.println("|----------------------|");
 			System.out.println("|----------------------|");
-			System.out.println("|                      cabeca");
-			System.out.println("|                olho direito");
-			System.out.println("|               olho esquerdo");
-			System.out.println("|                       nariz");
-			System.out.println("|                        boca");
-			System.out.println("|                      tronco");
-			System.out.println("|               braco direito");
-			System.out.println("|               braco esquerdo");
+			System.out.println("|                   (* - *)");
+			System.out.println("|                      o ");
+			System.out.println("|                  ___ | ___");
+			System.out.println("|                      |  ");
+			System.out.println("|                      -  ");
 		}
-		
+
 		if (partes == 9) {
-			System.out.println("------------------------");
 			System.out.println("|----------------------|");
 			System.out.println("|----------------------|");
-			System.out.println("|                      cabeca");
-			System.out.println("|                olho direito");
-			System.out.println("|               olho esquerdo");
-			System.out.println("|                       nariz");
-			System.out.println("|                        boca");
-			System.out.println("|                      tronco");
-			System.out.println("|               braco direito");
-			System.out.println("|               braco esquerdo");
-			System.out.println("|               perna direita");
+			System.out.println("|                   (* - *)");
+			System.out.println("|                      o ");
+			System.out.println("|                  ___ | ___");
+			System.out.println("|                      |  ");
+			System.out.println("|                      -  ");
+			System.out.println("|                     |  ");
+			System.out.println("|                     |  ");
 		}
-		
+
 		if (partes == 10) {
-			System.out.println("------------------------");
 			System.out.println("|----------------------|");
 			System.out.println("|----------------------|");
-			System.out.println("|                      cabeca");
-			System.out.println("|                olho direito");
-			System.out.println("|               olho esquerdo");
-			System.out.println("|                       nariz");
-			System.out.println("|                        boca");
-			System.out.println("|                      tronco");
-			System.out.println("|               braco direito");
-			System.out.println("|               braco esquerdo");
-			System.out.println("|               perna direita");
-			System.out.println("|               perna esquerda");
+			System.out.println("|                   (* - *)");
+			System.out.println("|                      o ");
+			System.out.println("|                  ___ | ___");
+			System.out.println("|                      |  ");
+			System.out.println("|                      -  ");
+			System.out.println("|                     | | ");
+			System.out.println("|                     | | ");
 		}
 	}
 
